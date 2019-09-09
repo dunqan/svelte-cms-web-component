3f87514c59f0fb1bea75d7e23af6ea01b88c884a
dunqan <dunqan@w9.pl>
2019-09-09 23:21:00 +0200

    initial

---
+++ src/App.svelte
@@ -0,0 +1,64 @@
+<svelte:options tag="svelte-search" />
+
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	export let cxApi;
+	let queryString = '';
+	let results = [];
+	let subscription;
+
+	onMount(() => {
+		setTimeout(() => {
+			if (cxApi) {
+				subscription = cxApi.searchbox
+      			.getSuggestionResults()
+      			.subscribe(res => {
+        			results = res;
+      			});
+			};
+		});
+	});
+
+	onDestroy(() => { 
+		if (subscription) {
+			subscription.unsubscribe();
+		}
+	});
+
+	function search(query) {
+		query = typeof query === 'string' ? query : queryString;
+		cxApi.routing.go({ cxRoute: 'search', params: { query } });
+	}
+	
+	function suggestionSearch(query) {
+		if (cxApi) {
+			cxApi.searchbox.searchSuggestions(query, {});
+		}
+	}
+
+    $: suggestionSearch(queryString);
+</script>
+
+<style>
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  color: red;
+  font-size: 0.8rem;
+  cursor: pointer;
+}
+</style>
+
+<input bind:value={queryString} placeholder="Search...">
+<button on:click={search}>
+	Search {queryString}
+</button>
+<ul>
+	{#each results as { value }}
+		<li on:click={()=>search(value)}>
+			{value}
+  	   </li>
+	{/each}
+</ul>
